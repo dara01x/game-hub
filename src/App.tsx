8502c0f6f91f4ddfb8e6b75198d35510edecb410
dara01x <daramistefa30@gmail.com>
2024-08-31 23:52:15 +0300

    Adding a NavBar and a logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
-function App() {
+const App = () => {
   return (
     <>
-      <Button colorScheme="teal">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="red">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
-}
+};
 
 export default App;
