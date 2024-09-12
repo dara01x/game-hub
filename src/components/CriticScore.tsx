0df73befa4859b25e0b7555e685c45b7852be34b
dara01x <daramistefa30@gmail.com>
2024-09-12 15:36:27 +0300

    Displaying Crtitics Score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,18 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  let color = score >= 90 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <>
+      <Badge borderRadius={2} fontSize="14px" paddingX={2} colorScheme={color}>
+        {score}
+      </Badge>
+    </>
+  );
+};
+
+export default CriticScore;
